24bdb8f328bf316255cc0a93b212f468bd160d8a
ValterlaneGarcia <valterlanegarcia@gmail.com>
2026-06-06 22:37:07 -0300

    feat: add IServidor interface for payroll domain

---
+++ week-02/interfaces.ts
@@ -0,0 +1,14 @@
+export interface IServidor {
+    registro: number
+    nome: string
+    nascimento: string
+    admissao: string
+    termino: string
+    cargo: string
+}
+
+export interface Ifinanceiro {
+    rubrica: string
+    provento: number
+    desconto: number
+}
